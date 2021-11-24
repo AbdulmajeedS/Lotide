c1d60e1d99d2e8c2aebc98a8460cf42ca03ad5cb
AbdulmajeedS <abdulmajeed.sabriye777@gmai.com>
2021-11-24 04:45:38 +0000

    finished

---
+++ assertArraysEqual.js
@@ -0,0 +1,13 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(actual, expected) {
+  // actual is eqArray function
+  if (actual, expected) {
+    console.log("PASS");
+  } else {
+    console.log("FAIL");
+  }
+};
+
+
+module.exports = assertArraysEqual;
