014d35a6bcded352a4de287ac6ec307b3105ad7e
AbdulmajeedS <abdulmajeed.sabriye777@gmai.com>
2021-11-25 00:40:39 +0000

    Tv

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    //console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+
+  }
+
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  horror: "The Conjuring",
+  action: "Marvel's Avengers",
+};
+
+const findKeyByValue = function(obj, value) {
+  return Object.keys(obj).find(key => obj[key] === value);
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Conjuring"), "horor"); 
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Marvel's Avengers"), undefined);
