e315857f0eebad6c6d16be6352f757ec649fa907
AbdulmajeedS <abdulmajeed.sabriye777@gmai.com>
2021-11-29 23:15:57 +0000

    finish

---
+++ test/tailsTest.js
@@ -0,0 +1,18 @@
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns 2 for length", () => {
+    assert.strictEqual(result.length, 2);
+  });
+
+  it("returns 'Lighthouse' as first string of tail(['Hello', 'Lighthouse', 'Labs'])", () => {
+    assert.strictEqual(result[0], "Lighthouse"); 
+  });
+
+  it("rreturns 'Labs' as second string of tail(['Hello', 'Lighthouse', 'Labs'])", () => {
+    assert.strictEqual(result[1], "Labs"); 
+  });
+
+});
